5e197d8994c814a8e1d246d8e02b6ed3f457e831
akika47 <61090999+akika47@users.noreply.github.com>
2023-11-14 12:19:18 +0100

    Add files via upload

---
+++ infoJegyzet_11_14/Lift.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace infoJegyzet_11_14
+{
+    internal class Lift
+    {
+        DateTime ido;
+        int sorszam;
+        int induloSzint;
+        int celSzint;
+
+        public Lift(DateTime ido, int sorszam, int induloSzint, int celSzint)
+        {
+            this.ido = ido;
+            this.sorszam = sorszam;
+            this.induloSzint = induloSzint;
+            this.celSzint = celSzint;
+        }
+
+        public DateTime Ido { get => ido; }
+        public int Sorszam { get => sorszam; }
+        public int InduloSzint { get => induloSzint; }
+        public int CelSzint { get => celSzint; }
+    }
+}
---
+++ infoJegyzet_11_14/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Timers;
+using System.Globalization;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using infoJegyzet_11_14;
+
+namespace infoJegyzet_11_14
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+
+            List<Lift> LiftAdatok = new List<Lift>();
+
+            using (StreamReader sr = new StreamReader("lift.txt"))
+            { 
+
+                while (!sr.EndOfStream)
+                {
+                    string sor = sr.ReadLine();
+                    string[] split1 = sor.Split(' ');
+                    Lift h = new Lift(Convert.ToDateTime(split1[0]), Convert.ToInt32(split1[1]), Convert.ToInt32(split1[2]), Convert.ToInt32(split1[3]));
+                    LiftAdatok.Add(h);
+                }
+                sr.Close();
+            }
+            // int sorsz;
+
+            int össz = 0;
+            int mentek;
+            for (int i = 0; i < LiftAdatok.Count; i++)
+
+            {
+                mentek = (LiftAdatok[i].InduloSzint + LiftAdatok[i].CelSzint);
+                if (mentek > 0) {
+                    össz++;
+                }
+
+            }
+            Console.WriteLine($"3.feladat: Összes lifthasználat: {össz}");
+
+            DateTime vegzes = LiftAdatok[össz - 1].Ido;
+            DateTime kezdes = LiftAdatok[0].Ido;
+
+            Console.WriteLine($"4.feladat: Időszak: {kezdes} - {vegzes}");
+
+            Console.WriteLine($"Célszint max: {LiftAdatok.Max(x => x.CelSzint)}");
+
+            Console.WriteLine("Kártya száma: ");
+            int kartyaSzam;
+ 
+            if (int.TryParse(Console.ReadLine(), out kartyaSzam) == false)
+            {
+                kartyaSzam = 5;
+            }
+   
+            Console.WriteLine("Célszint száma: ");
+            int celszintSzam;
+            if (int.TryParse(Console.ReadLine(), out celszintSzam) == false)
+            {
+                celszintSzam = 5;
+            }
+
+        }
+    }
+}
